833c765000f90cf0e5c2a83eb434c2c8413a1291
Jonphy Chen <jonphychen@gmail.com>
2019-11-12 20:12:55 +0800

    1.0.0

---
+++ B_Category/B_Category/CTMediator+B.swift
@@ -0,0 +1,18 @@
+//
+//  CTMediator+B.swift
+//  B_Category
+//
+//  Created by Jonphy on 2019/11/5.
+//  Copyright © 2019 Jonphy. All rights reserved.
+//
+
+import Foundation
+import CTMediator
+
+public extension CTMediator{
+    
+    func B_viewControllerWithContext(context:String) -> UIViewController {
+        let p = ["context":context]
+        return self.performTarget("B", action: "viewController", params: p, shouldCacheTarget: false) as! UIViewController
+    }
+}
